49a5004c308437ae0cc6d9b88dc694692378fef4
Brendon <brendonvbiljoen@gmail.com>
2020-02-16 09:31:35 +0200

    Changed home to generic path

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -7,5 +7,5 @@ mkdir $DEST
 echo $DEST
 
 sudo rsync --progress -aAX --delete --exclude=/dev/* --exclude=/proc/* --exclude=/sys/* --exclude=/tmp/*  \
-	--exclude=/run/* --exclude=/mnt/* --exclude=/home/brendon/android --exclude=/home/brendon/.android --exclude=/media/* --exclude="swapfile" --exclude="node_modules" --exclude="lost+found" --exclude=".cache" \
+	--exclude=/run/* --exclude=/mnt/* --exclude=$HOME/android --exclude=$HOME/.android --exclude=/media/* --exclude="swapfile" --exclude="node_modules" --exclude="lost+found" --exclude=".cache" \
 	--exclude="Downloads" --exclude="downloads" --exclude=".VirtualBoxVMs" --exclude=".ecryptfs" $SOURCE $DEST
