1ffae309d21ac71196238823c17ed56b3dbf4e6a
Brendon <brendonvbiljoen@protonmail.com>
2020-03-23 07:00:39 +0200

    Update polybar config

--- windowmanager/polybar/launch.sh
+++ windowmanager/polybar/launch.sh
@@ -1,4 +1,6 @@
 killall polybar
 for m in $(polybar --list-monitors | cut -d":" -f1); do
-    MONITOR=$m polybar --reload laptop &
+    MONITOR=$m polybar --reload top-left &
+    MONITOR=$m polybar --reload top-center &
+    MONITOR=$m polybar --reload top-right &
 done
