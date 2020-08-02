3c424d6b76657fa4fcf2ffb08626b91367830a67
Brendon <brendon@codehesion.co.za>
2020-08-02 15:52:08 +0200

    Update windowmanager for 3 monitors

--- windowmanager/polybar/launch.sh
+++ windowmanager/polybar/launch.sh
@@ -6,7 +6,9 @@ done
 
 MONITOR0="eDP-1"
 MONITOR1="HDMI-1"
+MONITOR2="DP-1"
 
 MONITOR=$MONITOR0 polybar --reload top-right &
 MONITOR=$MONITOR0 polybar --reload top-center &
 MONITOR=$MONITOR1 polybar --config=$HOME/.dotfiles/windowmanager/polybar/monitor-1.config --reload top-center &
+MONITOR=$MONITOR2 polybar --reload top-center &
