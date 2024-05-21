a61b61cb84104c3a7a01413433ce30170c6c7b84
Brendon van Biljoen <brendonvbiljoen@protonmail.com>
2024-05-21 13:02:31 +0200

    Add catppuccin as default nvim theme

--- nvim.lua/.config/nvim/lua/brendon/lazy/colors.lua
+++ nvim.lua/.config/nvim/lua/brendon/lazy/colors.lua
@@ -1,8 +1,17 @@
 return {
-    "nxstynate/oneDarkPro.nvim", 
-    priority = 1000,
-    config = function()
-
-    vim.cmd("colorscheme oneDarkPro")
-    end
+    {
+        "nxstynate/oneDarkPro.nvim",
+        -- priority = 1000,
+        -- config = function()
+        --     vim.cmd("colorscheme oneDarkPro")
+        -- end
+    },
+    {
+        "catppuccin/nvim",
+        name = "catppuccin",
+        priority = 1000,
+        config = function()
+            vim.cmd("colorscheme catppuccin-macchiato")
+        end
+    }
 }
